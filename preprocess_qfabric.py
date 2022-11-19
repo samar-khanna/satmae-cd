cd67a7323e5c9cfc2c4102e196d3ca9ad5f93927
Samar Khanna <samar.khanna@gmail.com>
2022-11-19 15:42:36 -0800

    Create subdir for tiling

--- preprocess_qfabric.py
+++ preprocess_qfabric.py
@@ -226,7 +226,10 @@ def create_tile(array_file, out_dir, tile_size=224, file_ext='tif'):
         components.insert(-1, f't{i}')
         out_f_name = '.'.join(components).replace('.tif', '.png')
 
-        out_f_path = os.path.join(out_dir, out_f_name)
+        tile_dir = os.path.join(out_dir, str(components[0]))
+        os.makedirs(tile_dir, exist_ok=True)
+
+        out_f_path = os.path.join(tile_dir, out_f_name)
 
         if len(tile.shape) == 3:
             tile = tile.transpose((1, 2, 0))  # (h, w, 3)
