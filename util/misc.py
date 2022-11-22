b3f927fb0e8f874a5d788056611ad9ce65b27b5f
Samar Khanna <samar.khanna@gmail.com>
2022-11-22 08:57:42 -0800

    Don't print the stupid cm values

--- util/misc.py
+++ util/misc.py
@@ -104,9 +104,10 @@ class MetricLogger(object):
     def __str__(self):
         loss_str = []
         for name, meter in self.meters.items():
-            loss_str.append(
-                "{}: {}".format(name, str(meter))
-            )
+            if not name.startswith('cm'):
+                loss_str.append(
+                    "{}: {}".format(name, str(meter))
+                )
         return self.delimiter.join(loss_str)
 
     def synchronize_between_processes(self):
