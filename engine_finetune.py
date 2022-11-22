07b56fabb6475c2c13f4a5ea8f850043dd4d14fd
Samar Khanna <samar.khanna@gmail.com>
2022-11-22 07:11:39 -0800

    Fix metrics

--- engine_finetune.py
+++ engine_finetune.py
@@ -301,7 +301,7 @@ def evaluate_segmenter(data_loader, model, device):
         batch_size = images.shape[0]
         metric_logger.update(loss=loss.item())
         metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        metric_logger.meters['acc3'].update(acc5.item(), n=batch_size)
+        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f} loss {losses.global_avg:.3f}'
